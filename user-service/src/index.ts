57a0b7ce54575e8bd57b792f94d7efab230790a1
d_star <deepanghan03@gmail.com>
2026-07-31 14:57:21 +0530

    user service exposed through server with server config

---
+++ user-service/src/index.ts
@@ -0,0 +1,29 @@
+import express, { type NextFunction, type Request, type Response } from "express";
+import { UserController } from "./controllers/user.controller.js";
+import dotenv from "dotenv";
+import { env } from "./config/env.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = env.PORT;
+
+app.use(express.json());
+
+const userController = new UserController();
+
+app.post("/api/user", userController.create);
+app.get("/api/user/:id", userController.get);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+
+    console.error(err);
+
+    res.status(500).json({
+        success: false,
+        message: "Internal Server Error"
+    });
+
+});
+
+app.listen(PORT, () => console.log(`user service running on ${PORT}`));
